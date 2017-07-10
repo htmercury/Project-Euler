d826be201c93468eaee4df38e68779822bd49de0
Ka Wong <kawong2020@u.northwestern.edu>
2017-07-09 22:13:07 -0500

    First intial attempt to Problem 3, more adjustments needed to account for high numbers

---
+++ js/problem_3.js
@@ -0,0 +1,23 @@
+// What is the largest prime factor of the number 600851475143 ?
+// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+var primedb = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]; // store prime values up to 100
+function findPrimeFactor(int) {
+    var factor = 0;
+    var ii = 0;
+    var acc = int;
+    while (factor === 0) {
+        if (acc%primedb[ii] === 0) {
+            acc /= primedb[ii];
+        }
+        else {
+            ii++
+        }
+        if (acc === primedb[ii]) {
+            factor = acc;
+        }
+    }
+    return factor;
+}
+console.log(findPrimeFactor(13195));
+// function above only works well for numbers less than 6 digits
+
